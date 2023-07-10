44603e1a0e0881eb2bbfe41af6c92b2f682cd27e
root <root@Prince-Puma>
2023-07-10 13:41:14 +0100

    a java program to print hello world

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,8 @@
-public class hello_world {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
+class hello_world
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, World!");
+	} /* end function */
+} /* end class hello_world */
 
